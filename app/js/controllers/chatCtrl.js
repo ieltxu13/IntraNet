cc028923abb952df1badc1327ae3c13a0304ed4d
Ieltxu Algañarás <ieltxu.alganaras@gmail.com>
2014-07-29 23:35:32 -0300

    primer commit branch chat, estructurita

---
+++ app/js/controllers/chatCtrl.js
@@ -0,0 +1,28 @@
+angular.module('intrApp').
+controller('chatCtrl',
+['$scope','Authentication','Chat',
+function($scope, Authentication, Chat) {
+
+  $scope.conectar = function() {
+
+  };
+  $scope.desconectar = function() {
+
+  };
+  $scope.getConectados = function() {
+
+  };
+  $scope.mostrarConectados = function() {
+
+  };
+  $scope.ocultarContactos = function() {
+
+  };
+  $scope.nuevaConversacion = function() {
+
+  };
+  $scope.ocultarConversacion = function() {
+
+  };
+
+}]);
---
+++ app/js/services/ChatService.js
@@ -0,0 +1,7 @@
+angular.module('intrApp')
+  .factory('Chat',['$scope','Authentication',
+  function($scope,Authentication){
+      return {
+        //ToDo Metodos
+      }
+  }])
